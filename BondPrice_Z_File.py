bab472c0df0a0fc88922833c19cbc2af1f0291cb
xw3113-lab <xw3113@columbia.edu>
2026-02-22 22:32:31 -0500

    Implement bond price calculation in getBondPrice_Z

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,14 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    
+    coupon = face * couponRate
+    bondPrice = 0
+    
+    cfs = [coupon] * len(times)
+    cfs[-1] += face
+    
+    for t, r, cf in zip(times, yc, cfs):
+        bondPrice += cf / ((1 + r) ** t)
+    
+    return(bondPrice)
