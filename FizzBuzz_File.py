93e500f0123b814a517e5a0815420a9e61f36694
xw3113-lab <xw3113@columbia.edu>
2026-02-22 22:45:47 -0500

    Implement FizzBuzz function to generate output list

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,15 @@
 
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 424, 'fizzbuzz']
-    return(v)
+    outlist = []
+    for i in range(start, finish + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            outlist.append("fizzbuzz")
+        elif i % 3 == 0:
+            outlist.append("fizz")
+        elif i % 5 == 0:
+            outlist.append("buzz")
+        else:
+            outlist.append(i)
+    
+    return(outlist)
