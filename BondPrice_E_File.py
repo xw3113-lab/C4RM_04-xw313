42ba09465b3d494ee48e4e8f4e4dc3cc5c767cae
xw3113-lab <xw3113@columbia.edu>
2026-02-22 20:46:38 -0500

    Add bond price and duration calculation function
    Implemented bond pricing calculation and duration logic.

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,23 @@
 
 
 def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+    y = y / ppy
+    n = m * ppy
+    coupon = face * couponRate / ppy
+    
+    sumPVCF = 0
+    sum_t_PVCF = 0
+    
+    for t in range(1, n + 1):
+        cf = coupon
+        if t == n:
+            cf += face
+        
+        pvcf = cf / ((1 + y) ** t)
+        sumPVCF += pvcf
+        sum_t_PVCF += t * pvcf
+    
+    bondDuration = (sum_t_PVCF / sumPVCF) / ppy 
+    
+    return(bondDuration)
+    
